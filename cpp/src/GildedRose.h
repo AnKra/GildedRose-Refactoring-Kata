444a37af5176d9eedfc265df11b0661f34e1cfe3
Kramer Angela (XC-AS/EDE4) <Angela.Kramer@de.bosch.com>
2025-02-07 10:57:11 +0100

    add clang-format config file and reformat all header and source files

--- cpp/src/GildedRose.h
+++ cpp/src/GildedRose.h
@@ -6,19 +6,22 @@ using namespace std;
 class Item
 {
 public:
-    string name;
-    int sellIn;
-    int quality;
-    Item(string name, int sellIn, int quality) : name(name), sellIn(sellIn), quality(quality) 
-    {}
+   string name;
+   int    sellIn;
+   int    quality;
+   Item(string name, int sellIn, int quality)
+      : name(name)
+      , sellIn(sellIn)
+      , quality(quality)
+   {
+   }
 };
 
 class GildedRose
 {
 public:
-    vector<Item> & items;
-    GildedRose(vector<Item> & items);
-    
-    void updateQuality();
-};
+   vector<Item>& items;
+   GildedRose(vector<Item>& items);
 
+   void updateQuality();
+};
--- cpp/test/cpp_googletest_approvaltest/GildedRoseGoogletestApprovalTests.cc
+++ cpp/test/cpp_googletest_approvaltest/GildedRoseGoogletestApprovalTests.cc
@@ -1,4 +1,4 @@
-// Include header files for test frameworks 
+// Include header files for test frameworks
 #include <gtest/gtest.h>
 #include <ApprovalTests.hpp>
 
@@ -7,27 +7,23 @@
 
 std::ostream& operator<<(std::ostream& os, const Item& obj)
 {
-    return os
-        << "name: " << obj.name
-        << ", sellIn: " << obj.sellIn
-        << ", quality: " << obj.quality;
+   return os << "name: " << obj.name << ", sellIn: " << obj.sellIn << ", quality: " << obj.quality;
 }
 
-TEST(GildedRoseApprovalTests, VerifyCombinations) {
+TEST(GildedRoseApprovalTests, VerifyCombinations)
+{
 
-    std::vector<string> names { "Foo" };
-    std::vector<int> sellIns { 1 };
-    std::vector<int> qualities { 1 };
+   std::vector<string> names{"Foo"};
+   std::vector<int>    sellIns{1};
+   std::vector<int>    qualities{1};
 
-    auto f = [](string name, int sellIn, int quality) {
-        vector<Item> items = {Item(name, sellIn, quality)};
-        GildedRose app(items);
-        app.updateQuality();
-        return items[0];
-    };
+   auto f = [](string name, int sellIn, int quality)
+   {
+      vector<Item> items = {Item(name, sellIn, quality)};
+      GildedRose   app(items);
+      app.updateQuality();
+      return items[0];
+   };
 
-    ApprovalTests::CombinationApprovals::verifyAllCombinations(
-            f,
-            names, sellIns, qualities);
-
+   ApprovalTests::CombinationApprovals::verifyAllCombinations(f, names, sellIns, qualities);
 }
